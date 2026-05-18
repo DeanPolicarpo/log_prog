b30318e9e6d12dbcd023b4e8ace97196103c4d39
Marlon <marlonlucio.academic@gmail.com>
2026-05-18 10:08:33 -0300

    Add a simple Python greeting and subtraction program

---
+++ desafios/marlon/exemplo.py
@@ -0,0 +1,17 @@
+n = input('Olá, qual seu nome?')
+print('Olá ' + n + ', seja bem-vindo! Meu nome é Marlon e estou aprendendo Python.')
+
+d1 = int(input('Me informe um número.'))
+
+print('Processando...')
+
+
+print('Obrigado ' + n + '! Agora irei fazer uma subtração.')
+
+d2 = int(input('Por favor, informe outro número.'))
+
+d = d1 - d2
+
+print('Processando...')
+
+print('Muito obrigado ' + n + '! O resultado foi :', d)
